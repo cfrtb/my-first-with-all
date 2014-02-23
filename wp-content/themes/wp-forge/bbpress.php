c5181023bee1166c77daf0e811eb9e00b247b594
Craig R <619hosting@gmail.com>
2014-02-23 10:30:48 -0800

    starting with wp-forge
    starting with wp-forge. remember to give credit to themeawesome

---
+++ wp-content/themes/wp-forge/bbpress.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Default Template for bbPress
+ *
+ * @package WordPress
+ * @subpackage WP_Forge
+ * @since WP-Forge 5.1.1
+ */
+get_header(); ?>
+
+		<div id="content" class="large-12 columns" role="main">
+        
+        	<?php if ( function_exists('yoast_breadcrumb') ) { yoast_breadcrumb('<ul class="breadcrumbs">','</ul>'); } ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+				<?php comments_template( '', true ); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/wp-forge/header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php wp_title('&#124;', true, 'right'); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<?php get_template_part( 'content', 'off_canvas' ); ?>
+
+	<?php if( get_theme_mod( 'wpforge_nav_position' ) == 'top') { ?>
+    	<?php get_template_part( 'content', 'nav' ); ?>
+    <?php } // end if ?>   
+
+	<div id="wrapper"> 
+          
+        <header id="header" class="row" role="banner"> 
+            <div class="site-header large-12 columns">
+            <?php if( get_theme_mod( 'wpforge_logo' ) ) { ?>
+                <div class="header-logo">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                        <img src="<?php echo get_theme_mod('wpforge_logo'); ?>" alt="<?php echo esc_attr( get_bloginfo('name', 'display') ); ?>">
+                    </a>
+                </div><!-- /.header-logo -->
+            <?php } // end if ?>
+                <div class="header-info">
+                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <h2 class="site-description"><small><?php bloginfo( 'description' ); ?></small></h2>                    
+                </div><!-- /.header-info -->
+             </div><!-- .site-header -->
+        </header><!-- #header -->
+            
+		<?php if( get_theme_mod( 'wpforge_nav_position' ) == 'normal') { ?>
+            <?php get_template_part( 'content', 'nav' ); ?>
+        <?php } // end if ?>
+        
+        <?php if( get_theme_mod( 'wpforge_nav_position' ) == 'sticky') { ?>
+            <?php get_template_part( 'content', 'nav' ); ?>
+        <?php } // end if ?> 
+        
+        <hr />         
+    
+        <section class="container row" role="document">
