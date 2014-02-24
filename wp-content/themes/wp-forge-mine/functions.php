c300c42bac0b3406606042b76dafc0633fb69875
Craig R <619hosting@gmail.com>
2014-02-23 18:59:27 -0800

    making it more my own

--- wp-content/themes/wp-forge-mine/functions.php
+++ wp-content/themes/wp-forge-mine/functions.php
@@ -184,11 +184,44 @@ function wpforge_scripts_styles() {
 
 	// Load all of our stylesheets
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/fonts/font-awesome.css' );	
-	wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css' );
-	wp_enqueue_style( 'foundation', get_template_directory_uri() . '/css/foundation.css' );
+	wp_enqueue_style( 'foundation', get_template_directory_uri() . '/css/app.css' );
 	wp_enqueue_style( 'wpforge', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'wpforge_scripts_styles', 0 );
+
+/**
+ * Create constants for directories accessed often
+ */
+define('IMAGES',  get_stylesheet_directory_uri().'/images/');
+
+
+//this line removes the existing Woocommerce codes
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+//This line adds a new action which adds a tag BEFORE the woocommerce "stuff"
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+
+//This line adds a new action which adds a tag AFTER the woocommerce "stuff"
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+//This line adds a new css section tag called "woogrid" BEFORE the woocommerce "stuff"
+function my_theme_wrapper_start() {
+    
+    
+	echo '<div id="content" class="large-8 columns" role="main">';
+    echo '<section id="woogrid">';
+    
+}
+
+//This line ends the new "woogrid" section
+function my_theme_wrapper_end() { 
+    
+    echo '</section>';
+    echo '</div>';
+    
+    
+}
 
 /**
  * Creates a nicely formatted and more specific title element text for output
